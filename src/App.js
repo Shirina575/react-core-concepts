2408bd6d6200cb99b08846a22e135c9a0475c712
Mst. Shirina Khatun <shirina575@gmail.com>
2021-09-11 07:43:05 +0600

    react component

--- src/App.js
+++ src/App.js
@@ -2,24 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const person = {
+    name: "Shirina",
+    title: "Student"
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{backgroundColor:'blue', color:'red'}}>Heading: {person.name + " " + person.title}</h1>
+        <p>I Am A React Developer</p>
+        <div style={{display:'flex'}}>
+          <Person></Person>
+          <Person></Person>
+        </div>
       </header>
     </div>
   );
+}
+
+function Person(){
+  const personStyle = {
+    border:'2px solid red', 
+    margin:'10px'
+  }
+  return (
+    <div style={personStyle}>
+      <h2 style={{backgroundColor:'green'}}>Name: Sakib Al Hasan</h2>
+      <h2>Name: Sakib 2</h2>
+    </div>
+  )
 }
 
 export default App;
